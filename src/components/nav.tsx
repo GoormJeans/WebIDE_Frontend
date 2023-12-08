1af6c855bce729461a3ae2d39d5de04df77512fc
childevnote <daniel011125@gmail.com>
2023-12-08 19:13:48 +0900

    Design: Background Color

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -5,24 +5,23 @@ function Nav() {
 
   const navigate = useNavigate();
 
-
   return (
     <nav className="w-full h-20 z-1">
-      <div className="px-5 w-auto h-full flex justify-between items-center bg-nav-color mx-5 rounded-xl">
-        <div className="text-3xl font-k2d left-0 ml-5 hover:cursor-pointer" onClick={() => window.location.replace('/')}>
+      <div className="px-5 w-auto h-full flex justify-between items-center bg-nav-color mx-5 mt-5 rounded-xl ">
+        <div className="text-3xl font-k2d left-0 ml-5 hover:cursor-pointer bg-transparent" onClick={() => navigate('/')}>
           JeansCode
         </div>
-        <div className="flex flex-row right-0">
-          <div className="font-k2d text-[#333333] text-lg mr-5 hover:cursor-pointer hover:underline" onClick={() => navigate("/algorithms")}>
+        <div className="flex flex-row right-0 bg-transparent">
+          <div className="font-k2d text-[#333333] text-lg mr-5 bg-transparent hover:cursor-pointer hover:underline " onClick={() => navigate("/algorithms")}>
             Algorithms
           </div>
-          <div className="font-k2d text-[#333333] text-lg mr-5 hover:cursor-pointer hover:underline" onClick={() => navigate("/mypage")}>
+          <div className="font-k2d text-[#333333] text-lg mr-5 bg-transparent hover:cursor-pointer hover:underline" onClick={() => navigate("/mypage")}>
             kim goorm
           </div>
-          <div className="font-k2d text-[#333333] text-lg mr-5 hover:cursor-pointer hover:underline">
+          <div className="font-k2d text-[#333333] text-lg mr-5 bg-transparent hover:cursor-pointer hover:underline">
             Logout
           </div>
-          <div className="font-k2d text-[#333333] text-lg mr-5 hover:cursor-pointer hover:underline" onClick={() => navigate("/settings")}>
+          <div className="font-k2d text-[#333333] text-lg mr-5 bg-transparent hover:cursor-pointer hover:underline" onClick={() => navigate("/settings")}>
             Settings
           </div>
         </div>
