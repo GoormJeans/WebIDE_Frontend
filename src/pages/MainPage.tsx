0bc1782b9b3310c22a847c92be849e3b36a3388e
childevnote <daniel011125@gmail.com>
2023-12-08 19:06:35 +0900

    Fix: fix bug

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MainPage = () => {
+  console.log('MainPage')
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MainPage
--- src/pages/Mypage.tsx
+++ src/pages/Mypage.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const Mypage = () => {
+const MyPage = () => {
+  console.log('MyPage')
   return (
     <div>
-      Mypage
+
     </div>
   )
 }
 
-export default Mypage
+export default MyPage
