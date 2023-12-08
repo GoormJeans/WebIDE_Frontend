d48e15694f1fcfff62ba604fc37297d7a7236f43
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2023-12-08 17:44:29 +0900

    Feat: Navigation - Router 설치 및 적용 - main(EditorPage) 분리 - 버튼 클릭 시 페이지 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import Main from './components/Main';
+import { Outlet, Route, Routes } from "react-router-dom";
+import Nav from "./components/nav";
+import MainPage from "./pages/MainPage";
+import DetailPage from './pages/DetailPage';
+import MyPage from "./pages/MyPage";
 
 function App() {
+  const Layout = () => {
+    return (
+      <div>
+        <Nav />
+        <Outlet />
+      </div>
+    )
+  }
   return (
     <div className='App'>
-      <Main />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path="algorithms" element={<DetailPage />} />
+          <Route path="mypage" element={<MyPage />} />
+          <Route path="detail" element={<DetailPage />} />
+          <Route path="settings" element={<DetailPage />} />
+
+        </Route>
+        <Route path="/editor" element={<Main />} />
+      </Routes>
+
     </div>
   );
 }
