be1310399fc111042c8730c2f238c45d782bb1d8
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2023-12-10 22:14:08 +0900

    Fix: AlgoList Key - 중첩 p태그 변경 - key 추가

--- src/components/MainPage/AlgoList.tsx
+++ src/components/MainPage/AlgoList.tsx
@@ -19,10 +19,10 @@ const AlgoList = () => {
     <div className="w-full bg-[#c4c4c4] h-[60px] flex flex-row items-center justify-between p-6 mb-5 rounded-xl hover:cursor-pointer hover:bg-white"
       onClick={() => navigate('/editor')}//클릭 시 editor 페이지로 이동, 나중에 id를 이용해서 문제 페이지로 이동시키는 방식으로 하면 될 듯
     >
-      <p className="bg-transparent flex flex-row">
+      <div className="bg-transparent flex flex-row">
         <p className="bg-transparent w-4">{prob.solved && "O"}</p>{/* 문제 풀었으면 O로 표시 */}
         <p className="bg-transparent ml-3">{prob.title}</p>
-      </p>
+      </div>
       <p className="bg-transparent">{prob.level}</p>
     </div>
   )
@@ -31,7 +31,7 @@ const AlgoList = () => {
     <div className="w-full h-full ">
       <div className="w-auto h-full flex flex-col justify-between items-center bg-transparent m-5 rounded-xl px-5 pt-5 border-2">
         {
-          probs.map((element) => <AlgoProbs prob={element} />)//아직 Pagination 미구현
+          probs.map((element) => <AlgoProbs prob={element} key={element.id}/>)//아직 Pagination 미구현
         }
       </div>
     </div>
