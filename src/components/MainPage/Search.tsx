408a853ed5b38e901ee9b09243ce0acaa7f7ffa3
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2023-12-11 19:54:29 +0900

    Design: Search Components Margin

--- src/components/MainPage/Search.tsx
+++ src/components/MainPage/Search.tsx
@@ -30,14 +30,14 @@ const Search = () => {
     <div className="w-full h-[70px]">
       <form className="w-auto h-full flex flex-row justify-between items-center bg-[#c4c4c4] m-5 rounded-xl py-5 shadow-xl whitespace-nowrap" onSubmit={handleSubmit}>
         {/* 레벨 선택에 따라 redux에 level state 변경 */}
-        <select className="pl-4 pr-2 h-full bg-black text-white ml-8 rounded-xl" onChange={handleFilterChange}>
+        <select className="pl-4 pr-2 h-full bg-black text-white ml-8 rounded-xl mr-5" onChange={handleFilterChange}>
           {levels.map((element: string) =>
             <option value={element} className="text-black whitespace-nowrap" key={element}>{element}</option>
           )}
         </select>
         <input
           type="text"
-          className="w-5/6 h-full rounded-xl px-4 bg-white"
+          className="w-5/6 h-full rounded-xl px-4 bg-white mr-5"
           value={searchValue}
           onChange={handleChange}
           placeholder="문제 검색"
