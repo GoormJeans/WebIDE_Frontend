8d8ce0c944a5fe0e65ebd6bb28c2522572e57341
childevnote <daniel011125@gmail.com>
2023-12-12 23:30:24 +0900

    Feat: Login, register Link header

---
+++ src/components/LoginSignupHeader.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+interface LoginSignupHeaderProps {
+  content: string;
+}
+
+const LoginSignupHeader: React.FC<LoginSignupHeaderProps> = ({ content }) => {
+  return (
+    <div className='grid justify-items-end'>
+      <Link to={content === "Login" ? "/login" : "/sign-up"} className='font-k2d underline text-lg bg-transparent pt-10 pr-7'>{content}</Link>
+    </div>
+  )
+}
+
+export default LoginSignupHeader
