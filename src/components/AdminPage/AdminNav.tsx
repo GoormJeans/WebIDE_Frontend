46a8a6d825a0d0d00b1dea832f2409e190996de0
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2023-12-12 22:05:37 +0900

    Feat: Admin Main Page

--- src/components/AdminPage/AdminNav.tsx
+++ src/components/AdminPage/AdminNav.tsx
@@ -9,7 +9,7 @@ const AdminNav = () => {
       <div className="w-auto h-full flex flex-row items-center bg-transparent mx-5 pl-5 whitespace-nowrap">
         <button className="h-[60px] items-center justify-between whitespace-nowrap hover:cursor-pointer text-white mr-5" onClick={()=>navigate('/admin')}>Home</button>
         <button className="h-[60px] items-center justify-between whitespace-nowrap hover:cursor-pointer text-white mr-5" onClick={()=>navigate('user')}>Users</button>
-        <button className="h-[60px] items-center justify-between whitespace-nowrap hover:cursor-pointer text-white mr-5" onClick={()=>navigate('algorithm')}>Problems</button>
+        <button className="h-[60px] items-center justify-between whitespace-nowrap hover:cursor-pointer text-white mr-5" onClick={()=>navigate('algorithm')}>Algorithms</button>
       </div>
     </div>
   )
