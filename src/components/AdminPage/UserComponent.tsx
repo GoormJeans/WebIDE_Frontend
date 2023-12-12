16527a6ac930249e6c87e7eb39bed70c5c89613f
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2023-12-12 22:05:56 +0900

    Feat: Admin User Page - Navigate to Delete Page

---
+++ src/components/AdminPage/UserComponent.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { UserType } from "../../pages/AdminUsersPage"
+import { useNavigate } from 'react-router-dom';
+
+const UserComponent: React.FC<UserType> = ({ id, nickname, email }) => {
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="w-full bg-[#c4c4c4] h-[60px] flex flex-row items-center justify-between p-6 mb-5 rounded-xl shadow-xl whitespace-nowrap">
+      <div className="bg-transparent flex flex-row justify-center">
+        <p className="bg-transparent w-4">{id}</p>
+        <p className="bg-transparent ml-3">{nickname}</p>
+        <p className="bg-transparent ml-3">{email}</p>
+      </div>
+      <button className="text-xl text-red-600" onClick={() => navigate(`/delete/${id}`)}>Delete</button>
+    </div>
+  )
+}
+
+export default UserComponent
--- src/pages/AdminUsersPage.tsx
+++ src/pages/AdminUsersPage.tsx
@@ -1,10 +1,44 @@
 import React from 'react'
+import UserChart from "../components/AdminPage/UserChart"
+import UserList from "../components/AdminPage/UserList";
+
+
+export interface UserType {
+  id: number;
+  nickname: string;
+  email: string;
+}
 
 const AdminUsersPage = () => {
+
+
+
+  // dummy users
+  const users: UserType[] = [
+    {
+      id: 1,
+      nickname: "test1",
+      email: "test1@gmail.com",
+    },
+    {
+      id: 2,
+      nickname: "test2",
+      email: "test2@naver.com",
+    },
+    {
+      id: 3,
+      nickname: "test3",
+      email: "test3@gmail.com",
+    }
+  ]
+
   return (
     <div className="w-full h-full">
-      <div className="px-5 w-auto h-full flex justify-between items-center bg-nav-color mx-5 mt-5 rounded-xl ">
-        AdminUsers
+      <div className="w-auto h-80 flex flex-col mx-5 p-5 whitespace-nowrap bg-nav-color shadow-xl rounded-xl">
+        <UserChart />
+      </div>
+      <div className="w-auto h-fit flex flex-col mx-5 mt-5 p-5 whitespace-nowrap bg-nav-color shadow-xl rounded-xl">
+        <UserList users={users} />
       </div>
     </div>
   )
