6a54105a25d6323ed25c1e30e3d84bb1f4e9cc4f
childevnote <daniel011125@gmail.com>
2023-12-13 11:40:56 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Main from './components/Main';
+
 import { Outlet, Route, Routes } from "react-router-dom";
 import Nav from "./components/nav";
 import MainPage from "./pages/MainPage";
 import DetailPage from './pages/DetailPage';
 import MyPage from "./pages/MyPage";
-import Login from './pages/Login';
-import Signup from './pages/Signup';
 import Main_c from './components/Main_c';
+
 
 function App() {
   const Layout = () => {
@@ -19,6 +19,7 @@ function App() {
     )
   }
   return (
+
     <div className='App'>
       <Routes>
         <Route path="/" element={<Layout />}>
@@ -28,10 +29,7 @@ function App() {
           <Route path="detail" element={<DetailPage />} />
           <Route path="settings" element={<DetailPage />} />
           <Route path="Search" element={<MainPage />} />
-
         </Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/sign-up" element={<Signup />} />
         <Route path="/editor" element={<Main />} />
       </Routes>
 
