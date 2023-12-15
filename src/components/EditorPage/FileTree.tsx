1de9886879fefdc6f61c75bad92d0236bf55a4bd
song-geun <brownmole01@naver.com>
2023-12-15 15:30:30 +0900

    fix : filetree UI

--- src/components/EditorPage/FileTree.tsx
+++ src/components/EditorPage/FileTree.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Tree from 'rc-tree';
 import '../../assets/file_tree/index.css';
 import '../../assets/file_tree/animation.less';
+
 const STYLE = `
 .rc-tree-child-tree {
   display: block;
@@ -111,20 +112,26 @@ function getTreeData() {
 
 
 //교체할 커스텀 icon
-const arrowPath =
-    'M869 487.8L491.2 159.9c-2.9-2.5-6.6-3.9-10.5-3.9h-88' +
-    '.5c-7.4 0-10.8 9.2-5.2 14l350.2 304H152c-4.4 0-8 3.6-8 8v60c0 4.4 3.' +
-    '6 8 8 8h585.1L386.9 854c-5.6 4.9-2.2 14 5.2 14h91.5c1.9 0 3.8-0.7 5.' +
-    '2-2L869 536.2c14.7-12.8 14.7-35.6 0-48.4z';
+const FolderCloseIcon =
+    'M64 480H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H288c-10.1 0-19.6-4.7-25.6-12.8L243.2 57.6C231.1 41.5 212.1 32 192 32H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64z';
+const FolderOpenIcon =
+    'M88.7 223.8L0 375.8V96C0 60.7 28.7 32 64 32H181.5c17 0 33.3 6.7 45.3 18.7l26.5 26.5c12 12 28.3 18.7 45.3 18.7H416c35.3 0 64 28.7 64 64v32H144c-22.8 0-43.8 12.1-55.3 31.8zm27.6 16.1C122.1 230 132.6 224 144 224H544c11.5 0 22 6.1 27.7 16.1s5.7 22.2-.1 32.1l-112 192C453.9 474 443.4 480 432 480H32c-11.5 0-22-6.1-27.7-16.1s-5.7-22.2 .1-32.1l112-192z';
 
-const getSvgIcon = (path: any, iStyle = {}, style = {}) => (
+const Fileicon =
+    'M320 464c8.8 0 16-7.2 16-16V160H256c-17.7 0-32-14.3-32-32V48H64c-8.8 0-16 7.2-16 16V448c0 8.8 7.2 16 16 16H320zM0 64C0 28.7 28.7 0 64 0H229.5c17 0 33.3 6.7 45.3 18.7l90.5 90.5c12 12 18.7 28.3 18.7 45.3V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V64z';
+const Fileiconview = '0 0 384 512'
+const FolderCloseView = '0 0 512 512';
+const FolderOpenView = '0 0 576 512';
+
+const getSvgIcon = (path: any, view: any, iStyle = {}) => (
     <i style={iStyle}>
         <svg
-            viewBox="0 0 1024 1024"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox={view}
             width="1em"
             height="1em"
             fill="currentColor"
-            style={{ verticalAlign: '-.125em', ...style }}
+            style={{ verticalAlign: '-.125em' }}
         >
             <path d={path} />
         </svg>
@@ -146,12 +153,11 @@ const File_tree = () => {
             return false;
         }
         if (obj.isLeaf) {
-            return;
+            return getSvgIcon(Fileicon, Fileiconview, { cursor: 'pointer', backgroundColor: 'white' });
         }
         return getSvgIcon(
-            arrowPath,
-            { cursor: 'pointer', backgroundColor: 'white' },
-            { transform: `rotate(${obj.expanded ? 90 : 0}deg)` },
+            obj.expanded ? FolderOpenIcon : FolderCloseIcon, obj.expanded ? FolderOpenView : FolderCloseView,
+            { cursor: 'pointer', backgroundColor: 'white' }
         );
     };
     if (treeRef === undefined)
@@ -167,7 +173,7 @@ const File_tree = () => {
                         defaultExpandAll
                         defaultExpandedKeys={defaultExpandedKeys}
                         motion={motion}
-                        style={{ height: '50vh' }}
+                        style={{ height: '100%' }}
                         treeData={getTreeData()}
                         //icon={ }
                         onSelect={onSelect}
