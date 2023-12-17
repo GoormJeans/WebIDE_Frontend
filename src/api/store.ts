88a1e65fc4960da9be6e3d25570f83a74a03e36e
childevnote <daniel011125@gmail.com>
2023-12-18 03:01:31 +0900

    Add: store about user info

--- src/api/store.ts
+++ src/api/store.ts
@@ -3,12 +3,15 @@ import scripts from './scripts';
 import algoprobs from "./algoprob";
 import filter from "./filter";
 import scriptsC from './scripts_c';
+import userSlice from './user';
+
 export const store = configureStore({
   reducer: {
     scripts: scripts,
     problems: algoprobs,//문제 목록 관리용
     filter: filter,//레벨 필터 설정용
     scriptsC: scriptsC,
+    user: userSlice,
   },
 });
 
