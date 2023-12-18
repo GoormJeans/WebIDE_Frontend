56484e5f49e356a26119c4f84f4bb3dd008c10ab
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2023-12-18 23:40:17 +0900

    Improve: Implement Algorithm Content by markdown

---
+++ src/components/AdminPage/AlgorithmContent.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+
+const AlgorithmContent: React.FC<{ contents: string; setContents: any }> = ({ contents, setContents }) => {
+  return (
+    <div className="flex flex-col ">
+      <div className="bg-transparent text-xl mb-3">Algorithm Contents</div>
+      <div className="flex flex-row">
+        <textarea id="algo_content" className="bg-white mb-5 mr-5 w-full rounded-xl p-3" value={contents} onChange={(e) => setContents(e.target.value)} />
+        <Markdown remarkPlugins={[remarkGfm]} className="w-full border-2 p-3 overflow-auto" >{contents}</Markdown>
+      </div>
+    </div>
+  )
+}
+
+export default AlgorithmContent
