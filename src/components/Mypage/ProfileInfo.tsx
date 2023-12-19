8832cd1801b05a1a725063c16b63adefc4a03b3c
childevnote <daniel011125@gmail.com>
2023-12-19 16:57:15 +0900

    Feat: change user data from dummy to api

--- src/components/Mypage/ProfileInfo.tsx
+++ src/components/Mypage/ProfileInfo.tsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
-import { user2 } from '../../types/DummyData';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { AppDispatch, RootState } from "../../api/store";
+import { setEmailValue, setNicknameValue, setAddressValue, setBioValue } from "../../api/user";
 
 const IMG_URL = 'https://url.kr/csn968'
-
 const HEAD_CSS = 'pr-3 text-2xl font-medium'
 const BUNDLE_CSS = 'bg-transparent font-k2d mt-3'
 
@@ -19,15 +22,35 @@ const InfoCard: React.FC<InfoCardProps> = ({ head, body }) => (
 );
 
 const ProfileInfo = () => {
+
+  const dispatch = useDispatch<AppDispatch>();
+  const userInfo = useSelector((state: RootState) => state.user);
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3003/api/user-info`);
+        // console.log('User information:', response.data);
+        dispatch(setEmailValue(response.data.email));
+        dispatch(setNicknameValue(response.data.nickname));
+        dispatch(setAddressValue(response.data.city));
+        dispatch(setBioValue(response.data.bio));
+      } catch (error) {
+        console.error('Error fetching user information:', error);
+      }
+    };
+
+    fetchUserInfo();
+  }, [dispatch]);
   return (
     <div className='profile-info flex flex-row'>
       <div className='flex flex-row'>
         <img className='bg-white w-36 h-36 rounded-full ml-10 mr-10 mt-5' src={IMG_URL} alt='fake-avartar' />
         <div className='mr-28'>
-          <InfoCard head="Nickname." body={user2.nickname} />
-          <InfoCard head="Add." body={user2.city} />
-          <InfoCard head="Email." body={user2.email} />
-          <InfoCard head="Bio." body={user2.bio} />
+          <InfoCard head="Nickname." body={userInfo.nicknameValue} />
+          <InfoCard head="Add." body={userInfo.cityValue} />
+          <InfoCard head="Email." body={userInfo.emailValue} />
+          <InfoCard head="Bio." body={userInfo.bioValue} />
         </div>
       </div>
     </div>
