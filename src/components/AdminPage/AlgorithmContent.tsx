14db63e825221716e9c79d1b42ec510f947b867a
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2023-12-19 10:18:16 +0900

    Fix: Tailwind CSS Typography setting

--- src/components/AdminPage/AlgorithmContent.tsx
+++ src/components/AdminPage/AlgorithmContent.tsx
@@ -9,7 +9,7 @@ const AlgorithmContent: React.FC<{ contents: string; setContents: any }> = ({ co
       <div className="bg-transparent text-xl mb-3">Algorithm Contents</div>
       <div className="flex flex-row">
         <textarea id="algo_content" className="bg-white mb-5 mr-5 w-full rounded-xl p-3" value={contents} onChange={(e) => setContents(e.target.value)} />
-        <Markdown remarkPlugins={[remarkGfm]} className="w-full border-2 p-3 overflow-auto" >{contents}</Markdown>
+        <Markdown remarkPlugins={[remarkGfm]} className="w-full border-2 p-3 overflow-auto prose lg:prose-xl" >{contents}</Markdown>
       </div>
     </div>
   )
