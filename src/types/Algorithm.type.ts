949fcd6d95c548e62df8aac016c9fbde9be62baf
childevnote <daniel011125@gmail.com>
2023-12-19 16:31:10 +0900

    Add: AlgorithmSolved type

--- src/types/Algorithm.type.ts
+++ src/types/Algorithm.type.ts
@@ -1,7 +1,13 @@
-export interface Algorithm{
+export interface Algorithm {
   id: number;
   name: string;
   url: string;
   tag: string;
   level: number;
+}
+
+export interface AlgorithmSolved extends Algorithm {
+  solved: boolean;
+  language: string;
+  stored: boolean;
 }
