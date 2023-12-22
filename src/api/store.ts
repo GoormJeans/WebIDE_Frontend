8f80810e7e2163cf49671f1e873ce0c4a7c1c119
childevnote <daniel011125@gmail.com>
2023-12-22 14:36:18 +0900

    Rename: algoprobmypage to algoprobs

--- src/api/store.ts
+++ src/api/store.ts
@@ -2,14 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import scripts from './scripts';
 import scriptsC from './scripts_c';
 import userSlice from './user';
-import algoprobsmypage from './algoprobmypage';
+import algoprobs from './algoprobs';
 
 export const store = configureStore({
   reducer: {
     scripts: scripts,
     scriptsC: scriptsC,
     user: userSlice,
-    problemsMypage: algoprobsmypage,
+    problems: algoprobs,
   },
 });
 
