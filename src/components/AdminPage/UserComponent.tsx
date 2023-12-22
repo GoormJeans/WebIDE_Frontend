78e24a75f914fd00228f6be7801f80882e5410f3
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2023-12-22 21:46:59 +0900

    Improve: User api code, user delete code

--- src/components/AdminPage/UserComponent.tsx
+++ src/components/AdminPage/UserComponent.tsx
@@ -1,20 +1,41 @@
-import React from 'react'
-import { UserType } from "../../pages/AdminUsersPage"
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
+import axios from "../../api/axios"
+import { UserInfo } from './../../types/UserInfo.type';
 
-const UserComponent: React.FC<UserType> = ({ id, nickname, email }) => {
+const UserComponent: React.FC<{id:string;nickname:string;email:string}> = ({ id, nickname, email }) => {
 
   const navigate = useNavigate();
+  const [deleteMod, setDeleteMod] = useState(false);
+
+  const handleDelete = async () => {
+    try {
+      const response = axios.delete(`/api/admin/user/${id}`);
+      console.log(response);
+    } catch (error) {
+      console.log("error", error);
+    }
+    window.location.replace("/admin/user");
+  }
 
   return (
-    <div className="w-full bg-[#c4c4c4] h-[60px] flex flex-row items-center justify-between p-6 mb-5 rounded-xl shadow-xl whitespace-nowrap">
-      <div className="bg-transparent flex flex-row justify-center">
-        <p className="bg-transparent w-4">{id}</p>
-        <p className="bg-transparent ml-3">{nickname}</p>
-        <p className="bg-transparent ml-3">{email}</p>
+    deleteMod ?
+      <div className="w-full bg-[#c4c4c4] h-[60px] flex flex-row items-center justify-between p-6 pr-0 mb-5 rounded-xl shadow-xl whitespace-nowrap">
+        <div className="w-full flex justify-end text-2xl">{nickname} 삭제 하시겠습니까?</div>
+        <div className="w-full flex justify-end items-center">
+          <button className="p-5 h-[60px] bg-red-500 w-fit text-white hover:bg-red-300 items-center" onClick={handleDelete}>YES</button>
+          <button className="p-5 h-[60px] bg-blue-500 w-fit text-white hover:bg-blue-300 items-center" onClick={() => setDeleteMod(false)}>NO</button>
+        </div>
       </div>
-      <button className="text-xl text-red-600" onClick={() => navigate(`/delete/${id}`)}>Delete</button>
-    </div>
+      :
+      <div className="w-full bg-[#c4c4c4] h-[60px] flex flex-row items-center justify-between p-6 mb-5 rounded-xl shadow-xl whitespace-nowrap">
+        <div className="bg-transparent flex flex-row justify-center whitespace-nowrap">
+          <p className="bg-transparent">{id}</p>
+          <p className="bg-transparent ml-3">{nickname}</p>
+          <p className="bg-transparent ml-3">{email}</p>
+        </div>
+        <button className="text-xl text-red-600" onClick={() => setDeleteMod(true)}>Delete</button>
+      </div>
   )
 }
 
--- src/components/AdminPage/UserList.tsx
+++ src/components/AdminPage/UserList.tsx
@@ -1,23 +1,44 @@
-import React from 'react'
-import { UserType } from "../../pages/AdminUsersPage"
+import React, { useState } from 'react'
 import UserComponent from "./UserComponent";
+import { UserInfo } from "../../types/UserInfo.type";
 
-const UserList: React.FC<{ users: UserType[] }> = ({ users }) => {
+const UserList: React.FC<{ users: UserInfo[] }> = ({ users }) => {
 
+  // Pagination용 데이터
+  const [page, setPage] = useState(1);
+  const limit = 10; // 한 번에 보여줄 문제 수
+  const offset = (page - 1) * limit; // 문제 번호 단위
+  const numPages = Math.ceil((users?.length || 0) / limit); // pagination 개수
+  if (numPages > 1) { users = users.slice(offset, offset + limit) } //1페이지 이상일 경우에만 작동
 
+  const pageButtons = new Array(numPages).fill(0);
 
   return (
-      <div className="w-auto h-full flex flex-col justify-between items-center  bg-nav-color m-5 rounded-xl px-5 pt-5 border-2 whitespace-nowrap">
-        {
-          users.map((element) =>
-            <UserComponent
-              key={element.id}
-              id={element.id}
-              nickname={element.nickname}
-              email={element.email}
-            />)
+    <div className="w-auto h-full flex flex-col justify-between items-center  bg-nav-color m-5 rounded-xl px-5 pt-5 border-2 whitespace-nowrap">
+      {
+        users && Array.isArray(users) &&
+        users.map((element) =>
+          <UserComponent
+            key={element.social_id}
+            id={element.social_id}
+            nickname={element.nickname}
+            email={element.email}
+          />)
+      }
+
+      {/* Pagination 1페이지 하나 이상 나올 경우에 보여주기*/}
+      {numPages > 1 && (<div className="justify-between text-white w-1/4 flex flex-row whitespace-nowrap">
+        <button onClick={() => setPage(page - 1)} disabled={page === 1} className="text-3xl">&lt;</button>
+        {pageButtons &&
+          pageButtons.map((item, idx) =>
+            <button key={idx + 1} onClick={() => setPage(idx + 1)} className={idx + 1 === page ? "rounded-full text-2xl border-1" : "text-2xl"}>
+              {idx + 1}
+            </button>
+          )
         }
-      </div>
+        <button onClick={() => setPage(page + 1)} disabled={page === numPages} className="text-3xl">&gt;</button>
+      </div>)}
+    </div>
   );
 }
 
