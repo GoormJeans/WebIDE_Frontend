1e73cf900ccee11a8a7520c940fef4f320c9d3aa
childevnote <daniel011125@gmail.com>
2023-12-25 00:13:07 +0900

    Rollback: withAuth Wrap Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Outlet, Route, Routes } from "react-router-dom";
 import Nav from "./components/nav";
 import MainPage from "./pages/MainPage";
@@ -13,14 +13,8 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import EditCode from './pages/EditCode';
 import EditUserInfo from './pages/EditUserInfo';
-import { AppDispatch } from './api/store';
-import { useDispatch } from 'react-redux';
-import { loginSuccess } from './api/auth';
-import { withAuth } from './components/WithAuth';
 
 function App() {
-  const dispatch = useDispatch<AppDispatch>();
-
   const ClickEvent = () => {
     const target: any = document.querySelector('#fileTreeRight');
     if (target !== null) {
@@ -47,44 +41,27 @@ function App() {
   }
   window.addEventListener("click", ClickEvent);
 
-  useEffect(() => {
-    const isLoggedIn = localStorage.getItem('isLoggedIn');
-    if (isLoggedIn) {
-      dispatch(loginSuccess(JSON.parse(isLoggedIn)));
-    }
-  }, [dispatch]);
-
-
-  const AuthenticatedMainPage = withAuth(MainPage);
-  const AuthenticatedMyPage = withAuth(MyPage);
-  const AuthenticatedEditUserInfoPage = withAuth(EditUserInfo);
-  const AuthenticatedAdminPage = withAuth(AdminPage);
-  const AuthenticatedAdminAlgoPage = withAuth(AdminAlgoPage);
-  const AuthenticatedAdminUsersPage = withAuth(AdminUsersPage);
-  const AuthenticatedAddAlgoPage = withAuth(AddAlgoPage);
-  const AuthenticatedEditCodePage = withAuth(EditCode);
-
   return (
     <div className='App'>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<AuthenticatedMainPage />} />
+          <Route path="/" element={<MainPage />} />
           <Route path="algorithms" element={<DetailPage />} />
-          <Route path="mypage" element={<AuthenticatedMyPage />} />
-          <Route path="mypage/edit" element={<AuthenticatedEditUserInfoPage />} />
+          <Route path="mypage" element={<MyPage />} />
+          <Route path="mypage/edit" element={<EditUserInfo />} />
           <Route path="detail" element={<DetailPage />} />
           <Route path="settings" element={<DetailPage />} />
 
           <Route path="admin" element={<AdminLayout />} >
-            <Route index element={<AuthenticatedAdminPage />} />
-            <Route path="algorithm" element={<AuthenticatedAdminAlgoPage />} />
-            <Route path="user" element={<AuthenticatedAdminUsersPage />} />
-            <Route path="algorithm/:id" element={<AuthenticatedAddAlgoPage />} />
+            <Route index element={<AdminPage />} />
+            <Route path="algorithm" element={<AdminAlgoPage />} />
+            <Route path="user" element={<AdminUsersPage />} />
+            <Route path="algorithm/:id" element={<AddAlgoPage />} />
           </Route>
-          <Route path="Search" element={<AuthenticatedMainPage />} />
+          <Route path="Search" element={<MainPage />} />
         </Route>
-        <Route path="/editor" element={<AuthenticatedEditCodePage />} />
-        <Route path=":id" element={<AuthenticatedEditCodePage />} />
+        <Route path="/editor" element={<EditCode />} />
+        <Route path=":id" element={<EditCode />} />
         <Route path="/login" element={<Login />} />
         <Route path="/sign-up" element={<Signup />} />
       </Routes>
