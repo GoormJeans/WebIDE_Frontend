c0b03a7c52dca1f0ad9a88d0a3daceb706915972
song-geun <brownmole01@naver.com>
2023-12-26 18:59:42 +0900

    fix : axios baseURL

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -7,13 +7,13 @@ export const init = ()=>{
 }
 export const instance : any =
     axios.create({
-    baseURL : 'http://goojeans-webide-docker.ap-northeast-2.elasticbeanstalk.com',
+    baseURL : 'https://goojeans-webide-docker.ap-northeast-2.elasticbeanstalk.com',
     headers : {
         'Authorization': str, // 헤더에 토큰을 포함시킵니다.
     }});
 export const instanceJSON : any = 
     axios.create({
-    baseURL : 'http://goojeans-webide-docker.ap-northeast-2.elasticbeanstalk.com',
+    baseURL : 'https://goojeans-webide-docker.ap-northeast-2.elasticbeanstalk.com',
     headers : {
         'Authorization': str, // 헤더에 토큰을 포함시킵니다.
         accept: 'application/json',"Content-Type": `application/json`,
