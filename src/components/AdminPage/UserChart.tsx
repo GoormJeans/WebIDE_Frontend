eb1adeb004be29e17d3aa2d4bfe0cb9d0f80d94c
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2023-12-27 02:11:23 +0900

    Feat: Dashboard

--- src/components/AdminPage/UserChart.tsx
+++ src/components/AdminPage/UserChart.tsx
@@ -1,17 +1,12 @@
 import { LineChart } from "@mui/x-charts"
 import React from 'react'
 
-const UserChart = () => {
+const UserChart: React.FC<{ userCounts: any }> = ({ userCounts }) => {
 
   // Users
   // x축에 날짜 지정
-  const today = new Date()
-  today.setDate(today.getDate() - 7);
-  const xUserLabels = Array(7).fill(0).map(_ => {
-    today.setDate(today.getDate() + 1)
-    return today.toLocaleDateString('en-US')
-  })
-  const userConnections = xUserLabels.map(() => Math.floor(Math.random() * 500)); // 사용자 관련한 접속 통계 가능할지 모르겠지만 
+  const xUserLabels: string[] = Object.keys(userCounts)
+  const userConnections: number[] = Object.values(userCounts); // 사용자 관련한 접속 통계 가능할지 모르겠지만 
   return (
     <div className="w-full h-full">
       <LineChart
