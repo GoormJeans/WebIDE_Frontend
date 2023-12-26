c7d814b2516932fb0150b11f9d1aba398cc647a9
song-geun <brownmole01@naver.com>
2023-12-26 15:02:39 +0900

    1226

--- src/api/EditfetchUrl.ts
+++ src/api/EditfetchUrl.ts
@@ -21,7 +21,7 @@ export const getSelect = async () =>{
 
     }catch(e)
     {
-        
+        return undefined;
     }
 }
 
@@ -31,13 +31,17 @@ export const Delete = async (key : string, number : number) =>{
             deletePathSuffix : key,
             algorithmId : number
         }
+        const accessToken = localStorage.getItem('AccessToken');
+        console.log(accessToken);
         const resp = await instance.post(requests.delete,JSON.stringify(data),{headers: {
+            'Authorization': `Bearer ${accessToken}`, // 헤더에 토큰을 포함시킵니다.
             accept: 'application/json',"Content-Type": `application/json`,
+            
           },} );
         return resp;
     }catch(e)
     {
-        return false;
+        return undefined;
     }
 }
 
@@ -48,13 +52,17 @@ export const Create = async (key : string, number : number) =>{
             createPath : Path,
             algorithmId : number
         }
+        const accessToken = localStorage.getItem('AccessToken');
+        console.log(accessToken);
         const resp = await instance.post(requests.create,JSON.stringify(data),{headers: {
+            'Authorization': `Bearer ${accessToken}`, // 헤더에 토큰을 포함시킵니다.
             accept: 'application/json',"Content-Type": `application/json`,
+            
           },} );
         return resp;
     }catch(e)
     {
-        
+        return undefined;
     }
 }
 export const dragNdrop = async (bkey : string ,key : string, number : number) =>{
@@ -65,13 +73,17 @@ export const dragNdrop = async (bkey : string ,key : string, number : number) =>
             afterPath : Path,
             algorithmId : number
         }
+        const accessToken = localStorage.getItem('AccessToken');
+        console.log(accessToken);
         const resp = await instance.patch(requests.modify,JSON.stringify(data),{headers: {
+            'Authorization': `Bearer ${accessToken}`, // 헤더에 토큰을 포함시킵니다.
             accept: 'application/json',"Content-Type": `application/json`,
+            
           },} );
         return resp;
     }catch(e)
     {
-        
+        return undefined;
     }
 }
 export const SaveFile = async () =>{
@@ -79,16 +91,23 @@ export const SaveFile = async () =>{
 
     }catch(e)
     {
-        
+        return undefined;   
     }
 }
 export const getFiletree = async ( number : string) =>{
     try{
-        const resp = await instance.get(requests.fetchFiletree+'1');
-        console.log(123);
+        const accessToken = localStorage.getItem('AccessToken');
+        const str = 'Bearer ' + accessToken;
+        console.log(str);
+        const resp = await instance.get(requests.fetchFiletree +  number, {
+            headers: {
+              'Authorization': str, // 헤더에 토큰을 포함시킵니다.
+            },
+          });
         return resp;
     }catch(e)
     {
         console.log(e);
+        return undefined;
     }
 }
