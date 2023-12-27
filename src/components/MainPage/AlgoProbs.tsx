eb55c83f5ad948d0d5eef9c8286d49653b3dd302
song-geun <brownmole01@naver.com>
2023-12-27 20:38:16 +0900

    test : result

--- src/components/MainPage/AlgoProbs.tsx
+++ src/components/MainPage/AlgoProbs.tsx
@@ -1,19 +1,15 @@
 import { useLocation, useNavigate, } from "react-router-dom";
 import { Algorithm } from "../../types/Algorithm.type";
 import React from "react";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "../../api/store";
-import {setProbno} from "../../api/FileTree";
 
 //알고리즘 리스트 항목
 const AlgoProbs: React.FC<{ prob: Algorithm }> = ({ prob }) => {
   const navigate = useNavigate();
-  const dispatch = useDispatch<AppDispatch>();
   const url = useLocation();
 
   return (
     <div className="w-full bg-[#c4c4c4] h-[60px] flex flex-row items-center justify-between p-6 mb-5 rounded-xl shadow-xl whitespace-nowrap hover:cursor-pointer hover:bg-white"
-      onClick={() => {dispatch(setProbno(prob.id));navigate(url.pathname === '/main' || url.pathname === '/algorithms' ? `/editor/${prob.id}` : `${prob.id}`)}}//클릭 시 editor 페이지로 이동, 나중에 id를 이용해서 문제 페이지로 이동시키는 방식으로 하면 될 듯
+      onClick={() => {navigate(url.pathname === '/main' || url.pathname === '/algorithms' ? `/editor/${prob.id}` : `${prob.id}`)}}//클릭 시 editor 페이지로 이동, 나중에 id를 이용해서 문제 페이지로 이동시키는 방식으로 하면 될 듯
     >
       <div className=" flex flex-row">
         <p className=" w-4">{prob.solved && "O"}</p>{/* 문제 풀었으면 O로 표시 */}
