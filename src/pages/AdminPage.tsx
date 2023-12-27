1ae16a5cc6c185ae18e7f6490c0e5c26397caff8
GangHyeokLee <33851859+GangHyeokLee@users.noreply.github.com>
2023-12-27 10:38:05 +0900

    Fix: Admin Dashboard

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -7,8 +7,7 @@ import axios from "../api/axios";
 const AdminPage = () => {
 
   const navigate = useNavigate();
-  const [usersCounts, setUsersCounts] = useState<string[]>([]);
-  const [algos, setAlgos] = useState<any[]>([])
+  const [counts, setCounts] = useState<any[]>([]);
 
   //언어별 정답 수 , 일별 가입자 수 가져오기
   useEffect(() => {
@@ -16,8 +15,7 @@ const AdminPage = () => {
     const getAdminDashboard = async ()=>{
       try {
         const request = await axios.get('/admin');
-        setUsersCounts([...request.data.data[0].userCounts])
-        setAlgos([...request.data.data[0].algos])
+        setCounts(request.data.data)
       } catch (error) {
         console.log(error);
       }
@@ -31,11 +29,11 @@ const AdminPage = () => {
     <div className="w-full h-full">
       <div className="w-auto h-80 flex flex-col mx-5 p-5 whitespace-nowrap bg-nav-color shadow-xl rounded-xl">
         <p className=" w-fit text-3xl  hover:cursor-pointer mb-3" onClick={() => navigate('user')}>Users</p>
-        <UserChart userCounts={usersCounts}/>
+        <UserChart counts={counts}/>
       </div>
       <div className="w-auto h-fit flex flex-col mx-5 p-5 whitespace-nowrap bg-nav-color shadow-xl rounded-xl mt-5">
         <p className=" w-fit text-3xl  hover:cursor-pointer mb-3" onClick={() => navigate('algorithm')}>Algorithms</p>
-        <LangChart algos={algos}/>
+        <LangChart counts={counts}/>
       </div>
     </div>
   )
