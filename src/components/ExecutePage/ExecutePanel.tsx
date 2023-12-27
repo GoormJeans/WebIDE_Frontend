14f98874bc44273602d263ce13a059d67702b8df
song-geun <brownmole01@naver.com>
2023-12-28 06:54:06 +0900

    Add : execute func

--- src/components/ExecutePage/ExecutePanel.tsx
+++ src/components/ExecutePage/ExecutePanel.tsx
@@ -1,11 +1,28 @@
 
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../api/store';
+import { execute, setTestCase } from '../../api/FileTree';
 
 const executePanel = () => {
-
+    const dispatch = useDispatch<AppDispatch>();
+    const setting: any = useSelector((state: RootState) => state.FileTree);
+    const handleChange = (e : any) =>{
+        dispatch(setTestCase(e.target.vale));
+    }
+    const handleExecute = () =>{
+        const data = {
+            algorithmId : setting.probno,
+            sourceCode : setting.sourcode,
+            filePath : setting.filePath,
+            fileExtension : setting.fileExtension,
+            testCase : setting.test_Case,
+        };
+        dispatch(execute(data));
+    }
   return (
     <div className="p-5 h-fit bg-[#AFAEAE] rounded-xl">
-        <input className="w-5/6"></input>
-        <div>제출</div>
+        <input value={setting.test_Case} onChange={handleChange} className="w-5/6"></input>
+        <div className="w-100% hover:bg-stone-400 pl-4 pr-4" onClick={handleExecute}>제출</div>
     </div>
   )
 }
