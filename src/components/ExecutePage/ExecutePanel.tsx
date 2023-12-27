443401f0e33eca8104985bf8903d2c50c59a00c4
song-geun <brownmole01@naver.com>
2023-12-28 06:38:15 +0900

    ExecutePanel

--- src/components/ExecutePage/ExecutePanel.tsx
+++ src/components/ExecutePage/ExecutePanel.tsx
@@ -4,8 +4,8 @@ const executePanel = () => {
 
   return (
     <div className="p-5 h-fit bg-[#AFAEAE] rounded-xl">
-
-      
+        <input className="w-5/6"></input>
+        <div>제출</div>
     </div>
   )
 }
